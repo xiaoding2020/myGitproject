a85c2e8a6ae6e7d3a02fad1657afe36742716dc0
xiaoding2020 <1607685418@qq.com>
2020-06-09 17:15:00 +0800

    把eclipse的工程推送到远程仓库

---
+++ git-project/src/com/xiao/git/UserGithub.java
@@ -0,0 +1,22 @@
+package com.xiao.git;
+
+import java.util.List;
+
+public class UserGithub {
+	public void addUser(int id,String username,String password) {
+		//add
+	}
+	
+	public void deleteUser(int id,String username,String password) {
+		//delete
+	}
+	public void updateUser(int id,String username,String password) {
+		//update
+	}
+	public List<Object> scanUser(int pageIndex,int pageSize){
+		//select
+		return null;
+		
+	}
+
+}

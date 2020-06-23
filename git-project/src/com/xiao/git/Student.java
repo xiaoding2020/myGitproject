07612946074e6f3bac14115d15e56d098ed291d5
xiaoding2020 <1607685418@qq.com>
2020-06-23 11:03:52 +0800

    提交成功

---
+++ git-project/src/com/xiao/git/Student.java
@@ -0,0 +1,27 @@
+package com.xiao.git;
+
+public class Student {
+	private int id;
+	private String username;
+	private String password;
+	public int getId() {
+		return id + 100;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}

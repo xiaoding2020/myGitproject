23357c4c31bfb556baf28a6ca5ccbeb267dbbae7
xiaoding2020 <65207450+xiaoding2020@users.noreply.github.com>
2020-06-23 11:12:29 +0800

    Update Student.java

--- git-project/src/com/xiao/git/Student.java
+++ git-project/src/com/xiao/git/Student.java
@@ -5,7 +5,7 @@ public class Student {
 	private String username;
 	private String password;
 	public int getId() {
-		return id + 100;
+		return id + 200;
 	}
 	public void setId(int id) {
 		this.id = id;
